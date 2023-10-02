3dd0e8de56731e4c24da9985e7d3fa9cd1d28cfd
niteshim0 <niteshim111@gmail.com>
2023-10-03 01:45:14 +0530

    project is refactored according to mvc pattern

--- index.js
+++ index.js
@@ -1,112 +1,24 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const fs = require("fs");
 const app = express();
-const route = express.Router();
+const router = express.Router();
 const PORT = 8000;
 
+const userRouter = require("./routes/user");
+const {connectMongoDB} = require("./connection");
+const { logReqRes } = require("./middlewares");
+
 //Establishing the Connection with MongoDB
-mongoose
-.connect("mongodb://127.0.0.1:27017/project-01")
-.then(()=>console.log("MongoDB connected"))
-.catch((err) => console.log("MongoDB Error",err));
+connectMongoDB("mongodb://127.0.0.1:27017/project-01")
+.then(() => {console.log("MongoDB connected!")});
 
 
-//Defining the Schema
-const userSchema = new mongoose.Schema(
-  {
-    firstName:{
-      type: String,
-      required : true,
-    },
-    lastName:{
-      type : String,
-    },
-    email:{
-      type:String,
-      required:true,
-      unique : true,
-    },
-    jobTitle: {
-      type:String
-    },
-    gender : {
-      type : String,
-    },
-  },
-  {timestamps:true},//take care of when a paticualar user is created and when it last updated
-);
-
-//Creating the model
-const User = mongoose.model("user",userSchema);
 
 //middlewares
 app.use(express.urlencoded({extended:false}));
+app.use(logReqRes("log.txt"));
 
-app.use((req,res,next)=> {
-  fs.appendFile(
-    "log.txt",
-    `\n${Date.now()} : ${req.ip} ${req.method} : ${req.path}\n`,
-    (err,data) => {
-      next();
-    }
-  );
-});
-//Error handler middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong!" });
-});
 //Routes
-
-app.get("/users",async(req,res)=> {
-  const  allDBUsers = await User.find({});
-  const html = `
-  <ul>
-  ${allDBUsers.map((user)=>`<li>${user.firstName}</li>`).join("")}
-  </ul> `
-});
-
-//REST API
-app.get("/api/users" , async(req,res)=>{
-  const allDBUsers = await User.find({});
-  return res.json(allDBUsers);
-});
-
-app.post("/api/users",async (req,res)=>{
-  const body  = req.body;
-  if(!body||!body.firstName||!body.lastName||!body.email||!body.gender||!body.jobTitle){
-    return res.status(400).json({msg:"All fields are required"});
-  }
-
-  const result = await User.create({
-     firstName : body.firstName,
-     lastName  : body.lastName,
-     email : body.email,
-     gender : body.gender,
-     jobTitle : body.jobTitle,
-  });
-  console.log("result",result);
-  return res.status(201).json({msg:"success"});
-});
-
-//finding the id and then making the changes
-app
-   .route("/api/users/:id")
-   .get( async(req,res) => {
-     const user  = await User.findById(req.params.id);
-     if(!user) return res.status(404).json({error : "user not found"});
-     return res.status(200).json(user);
-   })
-   .patch( async(req,res) => {
-    await User.findByIdAndUpdate(req.params.id,{lastName : "Changed"});
-    return res.status(200).json({msg : "success"});
-   })
-   .delete( async(req,res)=>{
-    await User.findByIdAndDelete(req.params.id);
-    return res.status(200).json({msg : "success"});
-   });
-
+app.use("/api/users",userRouter);
 app.listen(PORT,()=>console.log(`Server started at ${PORT}`));
 
 
